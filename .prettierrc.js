911bfc20110c84eaa45abebc9954d463c82643ff
Cruiz123 <carlos_rg11@hotmail.com>
2021-05-14 11:55:35 -0600

    :tada: - Carlos Ruiz - master - Inicializando proyecto

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: false,
+  trailingComma: 'all',
+  semi: false,
+  jsxSingleQuote: false,
+  tabWidth: 4,
+};
